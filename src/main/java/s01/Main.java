301713b91d54d9a383ec31a58b19afd99e0f12a4
Sebastian Raaphorst <sraaphorst@gmail.com>
2023-04-28 09:08:05 -1000

    More stuff.

--- src/main/java/s01/Main.java
+++ src/main/java/s01/Main.java
@@ -2,8 +2,10 @@ package s01;// *** GOODBYE, OUTDATED WORLD!
 
 public class Main {
     // Nope, nope, nope.
-    // C++ could do this in 2011. Java still can't.
-//    final var NotEvenLegal = 50;
+    // C++ could do this in C++11 published in 2011. Java still can't.
+    //    final var NotEvenLegal = 18;
+    // C++:
+    //    const auto DefinitelyLegal = 18;
 
     public static void main(final String[] args) {
         final var AndYetThisIsFine = 50;
